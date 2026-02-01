2c7cf84a7e6493c602d71be68ad1a5afde868661
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-02-01 11:15:02 +0100

    fixed problems

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,7 +4,10 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         //write your code here
-        if (password.length() < 10) {
+        if (password.length() < 10
+                || password.equals(repeatPassword)
+                || password != null
+                || repeatPassword != null) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
